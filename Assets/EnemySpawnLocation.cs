75207a3f650180b43b5966051b4536028ab6cf81
BhuvaneswariXcube <bhuvaneswari.gedela@xcubelabs.com>
2021-05-23 13:06:08 +0530

    Project on GalaxySpaceShooterGame

---
+++ Assets/EnemySpawnLocation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnLocation : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    float time = 0;
+    PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 2.0f && player != null)
+
+        {
+            float value = Random.Range(-2.36f, 2.36f);
+            GameObject enemyRef = Instantiate(enemyPrefab);
+            enemyRef.transform.position = new Vector3(value, 4.9f, 0);
+            time = 0;
+        }
+
+    }
+}
+
